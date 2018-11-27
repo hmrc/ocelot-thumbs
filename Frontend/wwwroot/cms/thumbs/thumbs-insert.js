16876cf6b30963dfeb005950d532f491694ed488
Osric Wilkinson <OsricWilkinson@users.noreply.github.com>
2018-11-27 11:04:09 +0000

    gd3104: Started

---
+++ Frontend/wwwroot/cms/thumbs/thumbs-insert.js
@@ -0,0 +1,15 @@
+
+/**
+ * addThumbs adds a thumbs block to the DOM. If target is a string
+ * it's used as a selector to find a DOM Element to attach to. Otherwise,
+ * target should be a DOM Element.
+ * 
+ * @param {selector} target 
+ */
+
+function addThumbs(target) {
+
+
+
+    
+}
