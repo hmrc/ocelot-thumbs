90c93bfe38987b78b1ca9fe09d039b9abb23d279
Ehtsham <ehtshamul.haq@hmrc.gsi.gov.uk>
2018-11-27 11:01:26 +0000

    project structure created for Thumbs API

---
+++ Backend/ThumbsApi/ThumbsApi/Models/Thumb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThumbsApi.Models
+{
+    public class Thumb
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [MaxLength(7)]
+        public string Pid { get; set; }  //pid
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public bool Rating { get; set; }        
+
+        public string Product { get; set; }  
+
+        [MaxLength(255)]
+        public string Group { get; set; }  //process
+
+        public string EndPoint { get; set; }  //last15 or endpoint
+    }
+}
---
+++ Backend/ThumbsApi/ThumbsApi/Services/Interfaces/IThumbsRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using ThumbsApi.Models;
+
+namespace ThumbsApi.Services.Interfaces
+{
+   public interface IThumbsRepository
+    {
+        /// <summary>
+        /// Adds a thumb to memory 
+        /// </summary>
+        /// <param name="thumb"></param>
+        void Add(Thumb thumb);
+
+        /// <summary>
+        /// Removes a thumb from memory
+        /// </summary>
+        /// <param name="thumb"></param>
+        void Delete(Thumb thumb);
+
+        /// <summary>
+        /// Gets a thumbs that meets the where criteria
+        /// </summary>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        Task<Thumb> GetAsync(Expression<Func<Thumb, bool>> where);
+
+        /// <summary>
+        /// Gets all thumbs
+        /// </summary>
+        Task<List<Thumb>> GetManyAsync();
+
+        /// <summary>
+        /// Gets all thumbs that meet the where criteria
+        /// </summary>
+        Task<List<Thumb>> GetManyAsync(Expression<Func<Thumb, bool>> where);
+
+        /// <summary>
+        /// Gets a number thumbs that meet the where criteria ordered by ordered
+        /// </summary>
+        Task<List<Thumb>> GetManyAsync<T>(Expression<Func<Thumb, bool>> where, Expression<Func<Thumb, T>> orderBy, int take);
+
+        /// <summary>
+        /// Save all changes in memory
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> SaveAsync();
+
+        /// <summary>
+        /// Update a thumb in memory
+        /// </summary>
+        /// <param name="thumb"></param>
+        void Update(Thumb thumb);
+    }
+}
