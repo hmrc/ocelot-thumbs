d2da8befd3601924296264f712094b9d4bd50abc
Ember Taylor <ember.taylor@digital.hmrc.gov.uk>
2018-12-03 11:34:44 +0000

    Update to allow publishing on app server

--- Backend/ThumbsApi/ThumbsApi/Contexts/Context.cs
+++ Backend/ThumbsApi/ThumbsApi/Contexts/Context.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,13 @@ namespace ThumbsApi.Contexts
 {
     public class Context:DbContext
     {
-        public Context(DbContextOptions<Context> options)
+        private ILogger<Context> _logger;
+
+        public Context(DbContextOptions<Context> options, ILogger<Context> logger)
             : base(options)
         {
-            if (!Database.EnsureCreated())
-            {
-                Database.Migrate();
-            }
+            _logger = logger;
+            Database.EnsureCreated();
         }
 
         public DbSet<Thumb> Thumbs { get; set; }
